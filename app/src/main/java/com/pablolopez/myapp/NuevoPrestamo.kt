b53536624829216f75173142d02f70f0d2ace390
Diego A <diegoacpa5c@gmail.com>
2018-05-20 17:40:06 -0600

    First steps de Nuevo Prestamo

---
+++ app/src/main/java/com/pablolopez/myapp/NuevoPrestamo.kt
@@ -0,0 +1,12 @@
+package com.pablolopez.myapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NuevoPrestamo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nuevo_prestamo)
+    }
+}
