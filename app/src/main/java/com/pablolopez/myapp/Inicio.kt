c8681abb1cc218b21403f3948af325d4ae55907b
Pablo Lopez <>
2018-05-22 17:10:02 -0600

    click en listView

--- app/src/main/java/com/pablolopez/myapp/Inicio.kt
+++ app/src/main/java/com/pablolopez/myapp/Inicio.kt
@@ -1,16 +1,16 @@
 package com.pablolopez.myapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
 
 import kotlinx.android.synthetic.main.activity_inicio.*
+import kotlinx.android.synthetic.main.content_inicio.*
 
 class Inicio : AppCompatActivity() {
 
@@ -20,16 +20,21 @@ class Inicio : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
         }
 
         val lista_notificaciones = findViewById<ListView>(R.id.notificaciones)
         lista_notificaciones.adapter = Inicio.CustomAdapter()
+
+        lista_notificaciones.setOnItemClickListener { adapterView, view, i, l ->
+            val intent = Intent(this, Notificacion::class.java)
+            startActivity(intent)
+        }
     }
     private class CustomAdapter(): BaseAdapter(){
         private val names = arrayListOf<String>(
-                "Deuda","Prestamo","Terminado"
+                "Diego","Mula","Que","Carrito","Te","Estoy","Haciendo"
         )
 
         override fun getCount(): Int {
@@ -61,8 +66,8 @@ class Inicio : AppCompatActivity() {
             }
 
             val viewHolder = rowMain.tag as ViewHolder
-            viewHolder.nameTextView.text = names.get(position)
-            viewHolder.positionText.text = "Descripicion"
+            viewHolder.nameTextView.text = "Tipo"
+            viewHolder.positionText.text = names.get(position)
 
             return rowMain
         }
---
+++ app/src/main/java/com/pablolopez/myapp/Notificacion.kt
@@ -0,0 +1,12 @@
+package com.pablolopez.myapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Notificacion : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notificacion)
+    }
+}
